6a43a23c4f456e4d4274ce22d2c838c8b0762059
Carlos Oliveros <oliveros.carlos.ro@gmail.com>
2024-11-26 15:31:46 -0600

    Terraform optional attributes removed

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,18 +16,18 @@ module "gcp-network" {
   source       = "terraform-google-modules/network/google"
   version      = "~> 9.3"
   project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
+  network_name = "gke-network-test"
 
   subnets = [
     {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
+      subnet_name   = "gke-subnet-test"
       subnet_ip     = "10.10.0.0/16"
       subnet_region = var.region
     },
   ]
 
   secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
+    "gke-subnet-test" = [
       {
         range_name    = var.ip_range_pods_name
         ip_cidr_range = "10.20.0.0/16"
